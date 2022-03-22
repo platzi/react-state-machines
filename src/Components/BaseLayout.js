0b5301d576470046a507b924c6dd9c99b08f57fd
Estefany Salas <estefanycss@gmail.com>
2022-03-22 12:28:11 -0500

    Mostrando metodos de estados

--- src/Components/BaseLayout.js
+++ src/Components/BaseLayout.js
@@ -6,6 +6,9 @@ export const BaseLayout = () => {
   const [state, send] = useMachine(bookingMachine);
 
   console.log('nuestra maquina', state);
+  console.log('matches true', state.matches('initial'));
+  console.log('matches false', state.matches('tickets'));
+  console.log('can', state.can('FINISH'));
   return (
     <div>Hola</div>
   );
